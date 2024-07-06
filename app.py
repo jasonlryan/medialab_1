76c6757704e76d87e0ca2b575073eaa0c0629fbd
Jason Ryan <jason_ryan@me.com>
2024-07-06 11:18:31 +0100

    Add all project files

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def chat():
 
     return Response(stream_with_context(generate()), content_type='text/event-stream')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#    app.run(debug=True)
